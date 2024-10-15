9b7971ce0e684b40918bda9f63bdd86673e941a3
GashawAkaNati <GashawAkaNati@gmail.com>
2024-10-15 11:46:02 +0300

    FirstComponent

---
+++ portfolio_app--/src/Components/GeneralInfo.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import '.../styles/InfoStyle.css';
+function GeneralInfo() {
+    return (
+        <>
+            <div className="SelfInfo">
+                <p>Hello world</p>
+            </div>
+        </>
+    );
+}
+export default GeneralInfo;
